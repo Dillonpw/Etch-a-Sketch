be05c0bdf6e51900564179da1b59bcca0e84ac6c
dillonpw <dillonpwalsh10@gmail.com>
2023-08-08 15:35:39 -0400

    style nearly finished, functions planned

---
+++ main.js
@@ -0,0 +1,7 @@
+//create color selector funciton
+
+//create screen fill function to replace background with selected color
+
+//create function to reset to screen color with erase
+
+//create on mousedown function to place color

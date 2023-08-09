a61ccfe9996a96059834a26b613aeb88648e4b5b
dillonpw <dillonpwalsh10@gmail.com>
2023-08-09 11:21:35 -0400

    fixed src error in html. add fill and erase func

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-//create color selector funciton
-
 //create screen fill function to replace background with selected color
+const color = document.getElementById('colors');
+const screenColor = document.getElementById('screen');
+const btnLeft = document.getElementById('btnLeft');
+const btnRight = document.getElementById('btnRight');
 
-//create function to reset to screen color with erase
+
+const savedColor = localStorage.getItem('selectedColor');
+if (savedColor){
+    screenColor.style.backgroundColor = savedColor;
+}
+
+btnLeft.addEventListener('click', () =>{
+    const selectedColor = color.value;
+    screenColor.style.backgroundColor = selectedColor;
+
+    localStorage.setItem('selectedColor', selectedColor);
+});
+
+btnRight.addEventListener('click', () =>{
+    screenColor.style.backgroundColor = 'antiquewhite';
+
+    localStorage.removeItem('selectedColor');
+})
 
 //create on mousedown function to place color

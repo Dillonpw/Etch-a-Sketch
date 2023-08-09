fd7ffa3d5bde92c16301402f2c94d9272a17e059
dillonpw <dillonpwalsh10@gmail.com>
2023-08-09 14:21:47 -0400

    functioning etch-a-sketch, adjusting styles

--- main.js
+++ main.js
@@ -4,23 +4,50 @@ const screenColor = document.getElementById('screen');
 const btnLeft = document.getElementById('btnLeft');
 const btnRight = document.getElementById('btnRight');
 
-
 const savedColor = localStorage.getItem('selectedColor');
-if (savedColor){
+if (savedColor) {
     screenColor.style.backgroundColor = savedColor;
 }
 
-btnLeft.addEventListener('click', () =>{
+btnLeft.addEventListener('click', () => {
     const selectedColor = color.value;
     screenColor.style.backgroundColor = selectedColor;
 
     localStorage.setItem('selectedColor', selectedColor);
 });
 
-btnRight.addEventListener('click', () =>{
-    screenColor.style.backgroundColor = 'antiquewhite';
+let isDrawing = false;
 
-    localStorage.removeItem('selectedColor');
-})
+screenColor.addEventListener('mousedown', () => {
+    isDrawing = true;
+});
 
-//create on mousedown function to place color
+screenColor.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
+screenColor.addEventListener('mousemove', drawPixel);
+
+function drawPixel(event) {
+    if (isDrawing) {
+        const selectedColor = color.value;
+        const dot = document.createElement('div');
+        dot.className = 'pixels';
+        dot.style.backgroundColor = selectedColor;
+
+        const rect = screenColor.getBoundingClientRect();
+        const offsetX = event.clientX - rect.left;
+        const offsetY = event.clientY - rect.top;
+
+        dot.style.left = offsetX + 'px';
+        dot.style.top = offsetY + 'px';
+
+        screenColor.appendChild(dot);
+    }
+}
+
+btnRight.addEventListener('click', () => {
+    const newColor = 'antiquewhite';
+    screenColor.style.backgroundColor = newColor;
+    localStorage.setItem('selectedColor', newColor);
+});
